f594c44b76114fd0d2266496cd28651b271538e3
BARU CHANDRA KIRAN <120393460+kiran-pyt@users.noreply.github.com>
2023-09-30 09:16:42 +0530

    Add files via upload

---
+++ ex1.m
@@ -0,0 +1,12 @@
+clc
+clear All
+close all
+
+
+y=zeros(1,5);
+
+x=[1,2,3,4,5];
+for i=1:5
+    y(i)=x(i)+10;
+end
+
---
+++ plot.m
@@ -0,0 +1,13 @@
+clc
+clear All
+close all
+
+
+y=zeros(1,5);
+
+x=[1,2,3,4,5];
+for i=1:5
+    y(i)=x(i)+10;
+end
+
+plot(x,y)
